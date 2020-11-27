478f965cf29456d88adfc28c92afd2029f52a8f5
lwooden <lowell.wooden@gmail.com>
2020-11-27 12:00:31 -0500

    first message

---
+++ src/components/Users/UserPicker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UsersPicker() {
+    return (
+        <select className='users-page'>
+            <option>Users</option>
+        </select>
+    )
+}
